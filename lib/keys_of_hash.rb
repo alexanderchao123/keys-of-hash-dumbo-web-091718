f78e8941eb0ca69afb61812493b1bce312d9e699
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-12 06:12:27 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,10 +3,11 @@ require 'pry'
 class Hash
   def keys_of(arguments)
     arr = Array.new()
-    binding.pry
+    # binding.pry
     self.each {|key, value| arr.push(key) if value == arguments}
+    return arr
   end
 end
 
-animals = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar"}
-animals.keys_of("hello")
+# animals = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar"}
+# animals.keys_of("hello")
