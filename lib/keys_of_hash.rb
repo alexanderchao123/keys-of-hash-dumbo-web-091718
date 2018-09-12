5a79d49e8fa2517e459a95440f44b74a4e1e439f
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-12 06:03:45 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,9 +2,9 @@ require 'pry'
 
 class Hash
   def keys_of(arg1, *arg2)
-    puts arg1
+    
   end
 end
 
-animals = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar"}
-animals.keys_of()
+# animals = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar"}
+# animals.keys_of("hello")
