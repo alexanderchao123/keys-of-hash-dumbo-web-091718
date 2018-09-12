05fbd44901b6867c2552614e11b13bcc26c27de4
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-12 06:09:50 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -6,6 +6,7 @@ class Hash
     # binding.pry
     self.each {|key, value| keys.push(key) if value == arguements}
   end
+  return keys
 end
 
 # animals = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar"}
