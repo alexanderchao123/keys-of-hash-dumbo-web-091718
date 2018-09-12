9ab017cb10619c9afccc6606d3daa6ce001c7c05
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-12 05:53:47 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,9 @@ require 'pry'
 class Hash
   def keys_of(arg1, *arg2)
     array = []
-    binding.pry
     return []
   end
 end
+
+animals = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar"}
+animals.keys_of
